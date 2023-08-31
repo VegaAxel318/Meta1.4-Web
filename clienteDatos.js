0a644ac63061ff2ccb54df7865de10326de16b7f
VegaAxel318 <AxelEVega318@gmail.com>
2023-08-31 06:45:27 -0700

    Segunda Version
    Uso de Callbacks

--- clienteDatos.js
+++ clienteDatos.js
@@ -1,7 +1,19 @@
 const servicio = require("./servicioDatos");
 
-servicio.getDatos(1,3000);
-servicio.getDatos(2,500);
-servicio.getDatos(3,4000);
-servicio.getDatos(4,700);
-servicio.getDatos(5,3500);
+//Primera version
+// servicio.getDatos(1,3000);
+// servicio.getDatos(2,500);
+// servicio.getDatos(3,4000);
+// servicio.getDatos(4,700);
+// servicio.getDatos(5,3500);
+
+//Uso de Callbacks
+servicio.getDatos(1,1000, function(){
+    servicio.getDatos(2,500,function(){
+        servicio.getDatos(3,1500,function(){
+            servicio.getDatos(4,700,function(){
+                console.log('Programa Terminado');
+            })
+        })
+    })
+});
