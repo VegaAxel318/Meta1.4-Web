9eb3d5910f3fda139d1bd568e2758d2621bec98a
VegaAxel318 <AxelEVega318@gmail.com>
2023-08-31 07:29:55 -0700

    Tercera Versión (Promises)
    Uso de Promises y async/await

--- clienteDatos.js
+++ clienteDatos.js
@@ -8,12 +8,26 @@ const servicio = require("./servicioDatos");
 // servicio.getDatos(5,3500);
 
 //Uso de Callbacks
-servicio.getDatos(1,1000, function(){
-    servicio.getDatos(2,500,function(){
-        servicio.getDatos(3,1500,function(){
-            servicio.getDatos(4,700,function(){
-                console.log('Programa Terminado');
-            })
-        })
-    })
-});
+// servicio.getDatos(1,1000, function(){
+//     servicio.getDatos(2,500,function(){
+//         servicio.getDatos(3,1500,function(){
+//             servicio.getDatos(4,700,function(){
+//                 console.log('Programa Terminado');
+//             })
+//         })
+//     })
+// });
+
+async function obtenerDatos() {
+    try {
+        await servicio.getDatos(1, 1000);
+        await servicio.getDatos(2, 500);
+        await servicio.getDatos(3, 1500);
+        await servicio.getDatos(4, 700);
+        console.log('Programa Terminado');
+    } catch(error) {
+        console.log(error);
+    }
+}
+
+obtenerDatos();
--- servicioDatos.js
+++ servicioDatos.js
@@ -1,11 +1,27 @@
 let finDeTransmisionDeDatos = function(id,callback){
-    console.log('Transferencia',id,'terminada');
-    callback();
+    return new Promise((resolve) => {
+        console.log('Transferencia',id,'terminada');
+        resolve();
+    });
+};
+
+let obtenDatosDeInternet = function(id, duracion) {
+    return new Promise((resolve) => {
+        console.log('Proceso', id, 'obteniendo datos de Internet');
+        setTimeout(() => {
+            finDeTransmisionDeDatos(id).then(() => {
+                resolve();
+            });
+        }, duracion);
+    });
+};
+
+const promesa = function() {
+    return new Promise(function(resolve,reject) {
+            resolve("Resultado esperado");
+            reject("Resultado no esperado");
+    });
 }
 
-let obtenDatosDeInternet = function(id,duracion,callback){
-    console.log('Proceso',id,'obteniendo datos de Internet');
-    setTimeout(finDeTransmisionDeDatos,duracion,id,callback);
-}
 
 module.exports.getDatos = obtenDatosDeInternet;
